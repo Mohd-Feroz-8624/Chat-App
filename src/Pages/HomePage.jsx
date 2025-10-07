78edf34a01415673db9fc8fa8621cc4fe4547c68
Mohd-Feroz-8624 <mohamedferoz2023@gmail.com>
2025-10-07 17:42:38 +0530

    side bar and assets are initialised

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,11 +11,11 @@ const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'} `}>
-        <SideBar />
-        <ChatContainer/>
-        <RightSideBar />
+        <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
       </div>
-      <button onClick={()=>setSelectedUser(!selectedUser)} className='absolute top-5 left-5 z-10 bg-blue-500 text-white px-3 py-1 rounded-md'>{selectedUser ? 'Hide' : 'Show'} Users</button>
+    
     </div>
   )
 }
